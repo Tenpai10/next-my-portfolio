1a07ab5fc936125748a269ec51f833985f068fd5
Tenpai10 <pransarbey@gmail.com>
2022-09-05 16:05:56 +0800

    🚀 meta: next-pwa integration

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,13 @@
-import { env } from "./src/env/server.mjs";
+import { env } from "./src/env/server.mjs"
+import pwaConfig from "next-pwa"
 
-/**
- * Don't be scared of the generics here.
- * All they do is to give us autocompletion when using this.
- *
- * @template {import('next').NextConfig} T
- * @param {T} config - A generic parameter that flows through to the return type
- * @constraint {{import('next').NextConfig}}
- */
-function defineNextConfig(config) {
-  return config;
-}
+const withPWA = pwaConfig({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+})
 
-export default defineNextConfig({
+export default withPWA({
   reactStrictMode: true,
   swcMinify: true,
-});
+})
